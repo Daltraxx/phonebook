007e0443e9b0894f9a4648fe2224f06dbc252a87
Dalton Pettus <daltpettus@gmail.com>
2025-04-28 13:11:14 -0700

    add get handler for querying person by id

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${phonebook.length} people - ${date}`);
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = phonebook.find(person => person.id === id)
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
