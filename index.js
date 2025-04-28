0e138330fbee7aef117253c30ba4f49e7578c5f2
Dalton Pettus <daltpettus@gmail.com>
2025-04-28 12:56:42 -0700

    add get handlers for /api/persons and /info

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
     res.send('<h1>This is a phonebook api!</h1>');
 })
 
+app.get('/api/persons', (req, res) => {
+    res.json(phonebook);
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date().toString();
+    res.send(`Phonebook has info for ${phonebook.length} people - ${date}`);
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
